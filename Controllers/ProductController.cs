156bcfc65468c796948072338adb9d8e0d5f7003
Milllle <milad_12@hotmail.com>
2025-05-23 20:28:47 +0200

    Add in-memory repository and ProductController

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using InventoryManager.Domain.Entities;
+using InventoryManager.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManager.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _repository.GetAllAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var product = await _repository.GetByIdAsync(id);
+            if (product is null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            await _repository.AddAsync(product);
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, Product product)
+        {
+            if (id != product.Id)
+                return BadRequest("ID mismatch");
+
+            await _repository.UpdateAsync(product);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _repository.DeleteAsync(id);
+            return NoContent();
+        }
+
+        [HttpGet("lowstock")]
+        public async Task<IActionResult> GetLowStock()
+        {
+            var lowStock = await _repository.GetLowStockAsync();
+            return Ok(lowStock);
+        }
+    }
+}
